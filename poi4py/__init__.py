6d65c8896219c09fbfc13687910c47dec76b9365
Pak Youngrok <pak.youngrok@gmail.com>
2018-07-11 01:07:10 +0900

    add classpath at runtime

--- poi4py/__init__.py
+++ poi4py/__init__.py
@@ -1,43 +1,49 @@
 import os
 
 import jpype
-from jpype import java
+import logging
+from jpype import java, JPackage
+from jpype.reflect import getDeclaredMethods
 
 
-classloader = None
-
+classpath_added = False
 
 def start_jvm():
-    global classloader
 
-    if jpype.isJVMStarted():
-        if not jpype.isThreadAttachedToJVM():
-            jpype.attachThreadToJVM()
-            java.lang.Thread.currentThread().setContextClassLoader(classloader)
-        return
+    if not jpype.isJVMStarted():
+        jpype.startJVM(jpype.get_default_jvm_path(),
+                       '-Dfile.encoding=UTF8',
+                       '-ea',
+                       '-Xmx1024m')
 
-    java_library_path = f'{os.path.dirname(__file__)}/java/'
-    classpath = os.pathsep.join(f'{java_library_path}/{lib}' for lib in [
-        'poi-3.17.jar',
-        'poi-excelant-3.17.jar',
-        'poi-ooxml-3.17.jar',
-        'poi-ooxml-schemas-3.17.jar',
-        'poi-scratchpad-3.17.jar',
-        'lib/commons-codec-1.10.jar',
-        'lib/commons-collections4-4.1.jar',
-        'lib/commons-logging-1.2.jar',
-        'lib/log4j-1.2.17.jar',
-        'ooxml-lib/xmlbeans-2.6.0.jar',
-        'ooxml-lib/curvesapi-1.04.jar',
-    ])
+    if not jpype.isThreadAttachedToJVM():
+        jpype.attachThreadToJVM()
 
-    jpype.startJVM(jpype.get_default_jvm_path(),
-                   f'-Djava.class.path={classpath}',
-                   '-Dfile.encoding=UTF8',
-                   '-ea',
-                   '-Xmx1024m')
+    global classpath_added
+    if not classpath_added:
+        add_classpaths([f'{os.path.dirname(__file__)}/java/{lib}' for lib in [
+            'poi-3.17.jar',
+            'poi-excelant-3.17.jar',
+            'poi-ooxml-3.17.jar',
+            'poi-ooxml-schemas-3.17.jar',
+            'poi-scratchpad-3.17.jar',
+            'lib/commons-codec-1.10.jar',
+            'lib/commons-collections4-4.1.jar',
+            'lib/commons-logging-1.2.jar',
+            'lib/log4j-1.2.17.jar',
+            'ooxml-lib/xmlbeans-2.6.0.jar',
+            'ooxml-lib/curvesapi-1.04.jar',
+        ]])
+        classpath_added = True
 
-    classloader = java.lang.Thread.currentThread().getContextClassLoader()
+
+def add_classpaths(paths):
+    URL = JPackage('java.net').URL
+    sysloader = JPackage('java.lang').ClassLoader.getSystemClassLoader()
+    method = [m for m in (getDeclaredMethods(JPackage('java.net').URLClassLoader)) if m.name == 'addURL'][0]
+    method.setAccessible(True)
+    for path in paths:
+        method.invoke(sysloader, [URL(f"file://{path}")])
 
 
 def open_workbook(filename, password=None, read_only=False):
@@ -46,8 +52,7 @@ def open_workbook(filename, password=None, read_only=False):
         return jpype.JPackage('org').apache.poi.ss.usermodel.WorkbookFactory.create(
             java.io.File(filename), password, read_only)
     except jpype.JavaException as e:
-        print(e.message())
-        print(e.stacktrace())
+        logging.exception(e)
         raise e
 
 
